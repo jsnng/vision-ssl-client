e463d9b7de2b0ab4427e31ae33eb7160250eab99
Jason Ng <7614175+jsnng@users.noreply.github.com>
2023-11-26 21:14:54 +1100

    recv listener added

--- build.rs
+++ build.rs
@@ -1,22 +1,26 @@
-fn main() {
-    const proto2: [&str; 7] = [
-        "messages_robocup_ssl_detection.proto",
-        "messages_robocup_ssl_geometry.proto",
-        "messages_robocup_ssl_wrapper.proto",
-        "messages_robocup_ssl_wrapper_tracked.proto",
-        "messages_robocup_ssl_detection_tracked.proto",
-        "messages_robocup_ssl_geometry_legacy.proto",
-        "messages_robocup_ssl_wrapper_legacy.proto"
-    ];
+// fn main() {
+//     let proto2: [&str; 7] = [
+//         "messages_robocup_ssl_detection.proto",
+//         "messages_robocup_ssl_geometry.proto",
+//         "messages_robocup_ssl_wrapper.proto",
+//         "messages_robocup_ssl_wrapper_tracked.proto",
+//         "messages_robocup_ssl_detection_tracked.proto",
+//         "messages_robocup_ssl_geometry_legacy.proto",
+//         "messages_robocup_ssl_wrapper_legacy.proto"
+//     ];
 
-    let root: String = "src/proto2/".to_owned();
+//     let root: String = "src/proto2/".to_owned();
 
-    let result = proto2.iter().map(|x| format!("{}{}", root, x))
-    .collect::<Vec<String>>();
+//     let result = proto2.iter().map(|x| format!("{}{}", root, x))
+//     .collect::<Vec<String>>();
     
-    protobuf_codegen::Codegen::new()
-    .cargo_out_dir("proto2")
-    .inputs(&result)
-    .include("src/proto2")
-    .run_from_script();
+//     let _ = protobuf_codegen_pure::Codegen::new()
+//     .out_dir("proto2")
+//     .inputs(&result)
+//     .include("src/proto2")
+//     .run();
+// }
+
+fn main() {
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+extern crate tokio;
 
+use tokio::net::UdpSocket;
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+
+    let socket = UdpSocket::bind("0.0.0.0:50513").await?;
+    let mut buffer = [0; 1204];
+
+    loop {
+        let (len, addr) = socket.recv_from(&mut buffer).await?;
+        println!("{:?} bytes recv from {:?}", len, addr)
+
+    }
 }
