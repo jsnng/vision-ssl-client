8727d2cb86e84fcf9b52d6a1590d2eab6fbc0cea
Jason Ng <7614175+jsnng@users.noreply.github.com>
2023-11-26 21:51:23 +1100

    sink current changes

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,51 @@ extern crate tokio;
 
 use tokio::net::UdpSocket;
 use std::io;
+use std::sync:Arc;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
 
-    let socket = UdpSocket::bind("0.0.0.0:50513").await?;
-    let mut buffer = [0; 1204];
+    let args = Command::new("vision-ssl-recv")
+    .version(crate_version!())
+    .arg(
+        Arg::new("local")
+        .short("l")
+        .long("local")
+        .required(true)
+        .value_name("IP:PORT")
+        .help()
+    )
+    .arg(
+        Arg::new("remote")
+        .short("r")
+        .long("remote")
+        .required(true)
+        .value_name("IP:PORT")
+        .help()
+    )
+    .get_matches();
 
-    loop {
-        let (len, addr) = socket.recv_from(&mut buffer).await?;
-        println!("{:?} bytes recv from {:?}", len, addr)
+    let local_ip_addr: SocketAddr = args
+    .get_one::<String>("local")
+    .unwrap()
+    .parse()
+    .expect("invalid local addr");
 
-    }
+    let remote_ip_addr: SocketAddr = args
+    .get_one::<String>("remote")
+    .unwrap()
+    .parse()
+    .expect("invalid remote addr");
+
+    num_cpus = num_cpus::get()
+    info!("{} cores available", num_cpus)
+
+    let main_loop = tokio::spawn(async move {
+        let mut buffer = [0; 1024];
+        loop {
+            let (len, addr) = udp
+        }
+    })
 }
+
